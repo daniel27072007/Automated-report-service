db43f829957c9c8f52ed243dda302b0edad3164d
daniel27072007 <daniel.belculfine@gmail.com>
2026-09-14 22:21:21 -0300

    pdf generator buffer

--- Pdf_Kit_Functions/PDF_Generator.js
+++ Pdf_Kit_Functions/PDF_Generator.js
@@ -8,7 +8,7 @@ const vendasDoBanco = [
     { clientName: 'Daniela Lima', amount: 230.15, status: 'paid' }
   ];
 
-export function createPDF (dataTable) {
+export function createPDF_FS (dataTable) {
     const doc = new PDFDocument
     doc.pipe(fs.createWriteStream('Weekly-Automated-Sales-Report.pdf'))
 
@@ -43,4 +43,48 @@ export function createPDF (dataTable) {
     doc.end()
 }
 
-createPDF(vendasDoBanco)
+//createPDF_FS(vendasDoBanco)
+
+export function CreatePDF_Buffer(dataTable) {
+    return new Promise((resolve, reject) => {
+        const doc = new PDFDocument
+        const buffers = []
+        doc.on('data', (chunk) => buffers.push(chunk))
+        doc.on('end', ()=>{
+            const pdfFinal = Buffer.concat(buffers)
+            resolve(pdfFinal)
+        })
+        doc.on('error', (err) => reject(err))
+
+        doc.fontSize(22).fillColor('#333333').text('Weekly Automated Sales Report', 50, 50)
+        doc.moveTo(50, 85).lineTo(550, 85).strokeColor('black').stroke()
+
+        let yActual = 110;
+        doc.fontSize(12).fillColor('black');
+        doc.text('clients', 50, yActual, { bold: true });
+        doc.text('amounts', 300, yActual);
+        doc.text('statuses', 450, yActual);
+        doc.moveTo(50, 125).lineTo(550, 125).strokeColor('#aaaaaa').stroke();
+
+        yActual = 140
+        let totalAmount = 0
+        dataTable.forEach((element) => {
+            doc.fontSize(11).fillColor('#555555');
+            doc.text(element.clientName, 50, yActual);
+            doc.text(`$ ${element.amount.toFixed(2)}`, 300, yActual);
+            doc.text(element.status.toUpperCase(), 450, yActual);
+            if(element.status.toUpperCase() === 'PAID'){
+                totalAmount += element.amount
+            }
+            yActual += 25;
+        });
+
+        doc.moveTo(50, yActual).lineTo(550, yActual).strokeColor('#aaaaaa').stroke();
+        yActual += 25
+        doc.text('TOTAL:', 50, yActual, { bold: true });
+        doc.text(`$ ${totalAmount.toFixed(2)}`, 300, yActual);
+
+        doc.end()
+
+    })
+}
