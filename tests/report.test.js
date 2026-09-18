a44821a40bcdef3319be93656792365cd034c4af
daniel27072007 <daniel.belculfine@gmail.com>
2026-09-18 19:23:07 -0300

    created folder and js for the integration tests

---
+++ tests/report.test.js
@@ -0,0 +1,8 @@
+import supertest from "supertest";
+import app from '../src/app.js'
+
+describe('Intergration tests - report routes', () => {
+    it('should respond with 200 when sending the report manualy', () => {
+        console.log("the test should run here")
+    })
+})
