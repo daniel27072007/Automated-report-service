26ff1c8f30e9a2d0d3a8b477960bb4d66bcb135e
daniel27072007 <daniel.belculfine@gmail.com>
2026-09-18 22:14:18 -0300

    integration test - done

--- tests/report.test.js
+++ tests/report.test.js
@@ -39,4 +39,35 @@ describe('Intergration tests - report routes', () => {
         expect(createdLog).toBeTruthy()
         expect(createdLog.status).toBe('SUCCESS')
     })
+
+    it('should respond with 200 and return a PDF buffer on preview route', async () => {
+        await salesModel.create({
+            clientName: "PREVIEW CORP",
+            amount: 50.00,
+            status: "paid"
+        });
+
+        const response = await supertest(app)
+            .get('/api/reports/preview')
+            .send();
+
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toBe('application/pdf');
+        expect(response.headers['content-disposition']).toContain('inline; filename=weekly-report.pdf');
+    });
+
+    it('should respond with 200 and return the list of logs on status route', async () => {
+        await reportLogModel.create({ status: 'SUCCESS' });
+        await reportLogModel.create({ status: 'FAILED', errorMessage: 'API Error' });
+
+        const response = await supertest(app)
+            .get('/api/reports/status')
+            .send();
+
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toBe(2);
+        
+        expect(response.body[0].status).toBeTruthy(); 
+    });
 })
